287e61cb9bb2237ff049a14b2dec9e5e37bfbd1b
masx200 <34191203+masx200@users.noreply.github.com>
2019-08-22 13:48:10 +0800

    使用Proxy实现对Object的深度观察,检测跟踪对象属性的变化的小工具

---
+++ deepobserveagent.js
@@ -0,0 +1,32 @@
+"use strict";
+export function isvalidobject(a) {
+  return a instanceof Object && typeof a === "object";
+}
+export default function observedeepagent(target, callback) {
+  if (typeof Proxy !== "function") {
+    setTimeout(() => {
+      throw Error("不支持Proxy!");
+    }, 0);
+    return target;
+  } else {
+    if (isvalidobject(target)) {
+      return new Proxy(target, {
+        set(t, k, v) {
+          callback(t, k, v);
+
+          Reflect.set(t, k, v);
+          return true;
+        },
+        get(t, k) {
+          var value = Reflect.get(t, k);
+          if (isvalidobject(value)) {
+            return observedeepagent(value, callback);
+          }
+          return value;
+        }
+      });
+    } else {
+      return target;
+    }
+  }
+}
